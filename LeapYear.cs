6fe4ff582b6a0794de2346108ad147ac6233fbea
ashu1-12 <sadanshivashish122@gmail.com>
2023-09-03 14:39:05 +0530

    Is Leap Year

---
+++ LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class LeapYear
+    {
+        public bool IsLeapYear(int year)
+        {
+            if(year%4 == 0) {
+                if(year%100 == 0)
+                {
+                    //for century years like 1900 ,2000
+                   if(year%400 == 0)
+                    { return true; }
+                   else { return false; }
+               
+                }
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
